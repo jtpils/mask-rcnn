fb5bfd7021aacc860dd903df82535f9945c1ad4f
bashirkazimi <kazimibashir907@gmail.com>
2019-02-14 16:24:20 +0100

    working model

--- src/model.py
+++ src/model.py
@@ -2059,7 +2059,7 @@ class MaskRCNN():
 
         # Add multi-GPU support.
         if config.GPU_COUNT > 1:
-            from mrcnn.parallel_model import ParallelModel
+            from parallel_model import ParallelModel
             model = ParallelModel(model, config.GPU_COUNT)
 
         return model
