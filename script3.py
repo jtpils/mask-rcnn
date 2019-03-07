b6df745e753c1e68c90d47bbcd64862b2d0070db
bashirkazimi <kazimibashir907@gmail.com>
2019-03-07 17:28:24 +0100

    bomb meiler barrow

---
+++ script3.py
@@ -0,0 +1,148 @@
+"""
+Mask R-CNN
+Train on the toy Balloon dataset and implement color splash effect.
+
+Copyright (c) 2018 Matterport, Inc.
+Licensed under the MIT License (see LICENSE for details)
+Written by Waleed Abdulla
+
+------------------------------------------------------------
+
+Usage: import the module (see Jupyter notebooks for examples), or run from
+       the command line as such:
+
+    # Train a new model starting from pre-trained COCO weights
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=coco
+
+    # Resume training a model that you had trained earlier
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=last
+
+    # Train a new model starting from ImageNet weights
+    python3 balloon.py train --dataset=/path/to/balloon/dataset --weights=imagenet
+
+    # Apply color splash to an image
+    python3 balloon.py splash --weights=/path/to/weights/file.h5 --image=<URL or path to file>
+
+    # Apply color splash to video using the last weights you trained
+    python3 balloon.py splash --weights=last --video=<URL or path to file>
+"""
+
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+from glob import glob
+# Root directory of the project
+# ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+# sys.path.append(ROOT_DIR)  # To find local version of the library
+from src.config import Config
+import src.model as modellib
+import src.utils as utils
+from src import harz3 as harz
+
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect balloons.')
+    parser.add_argument("command",
+                        metavar="<command>",
+                        help="'train' or 'splash'")
+    parser.add_argument('--dataset', required=False,
+                        metavar="/path/to/balloon/dataset/",
+                        help='Directory of the harz dataset')
+    parser.add_argument('--weights', required=False,
+                        metavar="/path/to/weights.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default='./log',
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    parser.add_argument('--image', required=False,
+                        metavar="path or URL to image",
+                        help='Image to apply the color splash effect on')
+    parser.add_argument('--video', required=False,
+                        metavar="path or URL to video",
+                        help='Video to apply the color splash effect on')
+    args = parser.parse_args()
+
+    # Validate arguments
+    # if args.command == "train":
+    #     assert args.dataset, "Argument --dataset is required for training"
+    # elif args.command == "splash":
+    #     assert args.image or args.video,\
+    #            "Provide --image or --video to apply color splash"
+
+    # print("Weights: ", args.weights)
+    # print("Dataset: ", args.dataset)
+    # print("Logs: ", args.logs)
+
+    # Configurations
+    if args.command == "train":
+        config = harz.HarzConfig()
+    else:
+        class InferenceConfig(harz.HarzConfig):
+            # Set batch size to 1 since we'll be running inference on
+            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+            GPU_COUNT = 1
+            IMAGES_PER_GPU = 1
+        config = InferenceConfig()
+    config.display()
+
+    # Create model
+    if args.command == "train":
+        model = modellib.MaskRCNN(mode="training", config=config,
+                                  model_dir=args.logs)
+    else:
+        model = modellib.MaskRCNN(mode="inference", config=config,
+                                  model_dir=args.logs)
+
+    if args.weights and args.weights.lower() == "last":
+        # Find last trained weights
+        print("Loading model from previous weight file!")
+        weights_path = model.find_last()
+        model.load_weights(weights_path, by_name=True)
+
+    # Select weights file to load
+    # if args.weights.lower() == "coco":
+    #     weights_path = COCO_WEIGHTS_PATH
+    #     # Download weights file
+    #     if not os.path.exists(weights_path):
+    #         utils.download_trained_weights(weights_path)
+    # elif args.weights.lower() == "last":
+    #     # Find last trained weights
+    #     weights_path = model.find_last()
+    # elif args.weights.lower() == "imagenet":
+    #     # Start from ImageNet trained weights
+    #     weights_path = model.get_imagenet_weights()
+    # else:
+    #     weights_path = args.weights
+
+    # # Load weights
+    # print("Loading weights ", weights_path)
+    # if args.weights.lower() == "coco":
+    #     # Exclude the last layers because they require a matching
+    #     # number of classes
+    #     model.load_weights(weights_path, by_name=True, exclude=[
+    #         "mrcnn_class_logits", "mrcnn_bbox_fc",
+    #         "mrcnn_bbox", "mrcnn_mask"])
+    # else:
+    #     model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    dataset_dirX='/notebooks/tmp/data/bomb_meiler_barrow_mrcnn/x'
+    dataset_dirY='/notebooks/tmp/data/bomb_meiler_barrow_mrcnn/ys/masks'
+    if args.command == "train":
+        harz.train(model,dataset_dirX,dataset_dirY,config)
+    elif args.command == "splash":
+        harz.detect_and_color_splash(model, image_path=args.image,
+                                video_path=args.video)
+    else:
+        print("'{}' is not recognized. "
+              "Use 'train' or 'splash'".format(args.command))
